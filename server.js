beb8f8129740aec34ddc7e0eb6332d3bb661af21
aidanclemente <aidanclemente@gmail.com>
2018-03-30 22:21:56 -0400

    heroku deployment and then some

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var mongoose = require("mongoose");
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
-// Require all models this will be moved with the routes
+// Require all models 
 var db = require("./models");
 
 var PORT = process.env.PORT || 3000;
@@ -30,9 +30,6 @@ app.use(express.static("public"));
 
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/mongoScraper");
-
-// Apparently, all we need to require is the routes, and the each file from there requires the others
-
 
 var scrapePage = require("./scripts/scrape.js");
 
@@ -81,7 +78,6 @@ app.get("/articles/:id", function(req, res) {
   })
   .populate("note")
   .then(function(dbHeadlineById) {
-    // If we were able to successfully find an Article with the given id, send it back to the client
     res.json(dbHeadlineById);
   })
   .catch(function(err) {
@@ -106,22 +102,14 @@ app.put("/articles/:id/:saved", function(req, res) {
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function(req, res) {
 
-  // Create a new note and pass the req.body to the entry
   db.Note.create(req.body)
     .then(function(dbNote) {
-      // IF a Note was created successfully, find one Article with and _id = req.params.id  
-
-      //Update the Article to be associated with the new Note { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-    
       return db.Headline.findOneAndUpdate({ _id: req.params.id }, {note: dbNote._id }, { new: true });
     })
-    // Since our mongoose query returns a promise, we can chain another .then which receives the result of the query
     .then(function(dbHeadline) {
-      // If we were able to successfully update an Article, send it back to the client
       res.json(dbHeadline);
     })
     .catch(function(err) {
-      // If an error occured, send it to the client
       res.json(err);
     });
 });
